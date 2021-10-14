07b9fd970fc4470a365c8aecf1bac118ff332a99
Danilo Nikolic <danilo@dramin-ns.com>
2021-10-14 15:11:44 +0200

    Add project files.

---
+++ WindowsServiceLive/Service1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsServiceLive
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer tm = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service starts at " + DateTime.Now);
+            tm.Elapsed += new ElapsedEventHandler(onElapsedTime);
+            tm.Enabled = true;
+            tm.Interval = 1000;
+        }
+
+        private void onElapsedTime(object sender, ElapsedEventArgs e)
+        {
+            WriteToFile("Service recalls at " + DateTime.Now);
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service stops at " + DateTime.Now);
+        }
+
+        private void WriteToFile(string message)
+        {
+            string folderpath = AppDomain.CurrentDomain.BaseDirectory + "ServiceLogPath";
+            
+            if (!Directory.Exists(folderpath))
+                Directory.CreateDirectory(folderpath);
+
+            string filepath = folderpath + @"\" + "logfile" + ".txt";
+
+            if (!File.Exists(filepath))
+            {
+                using(StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+    }
+}
